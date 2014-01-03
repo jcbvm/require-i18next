3fc3def63509ff3743478d2d7c3d65214b0e26b9
Jacob van Mourik <jacob.v.m@gmail.com>
2014-01-03 18:58:50 +0100

    Fixed issues loading resources multiple dirs

--- require-i18next/i18next.js
+++ require-i18next/i18next.js
@@ -1,16 +1,18 @@
 /**
  * RequireJS i18next Plugin
  * 
- * Version 0.3.0 (12-31-2013)
+ * Version 0.3.0 (01-03-2013)
  * Copyright 2013 Jacob van Mourik
  * Released under the MIT license
  */
-define(["i18next"], function(i18next) {
+define([ "i18next" ], function(i18next) {
     "use strict";
 
-    var plugin,
-        origResGetPath,
-        f = i18next.functions,
+    var plugin, 
+        defaultNamespace, 
+        supportedLngs, 
+        resStore = {}, 
+        f = i18next.functions, 
         o = i18next.options;
 
     plugin = {
@@ -28,78 +30,93 @@ define(["i18next"], function(i18next) {
         parseName: function(name) {
             var splitted = name.split(":");
             return {
-                module: splitted[0],
-                namespaces: splitted[1] ? splitted[1].split(",") : []
+                module : splitted[0],
+                namespaces : splitted[1] ? splitted[1].split(",") : []
             };
         },
 
         load: function(name, req, onload, config) {
-            var parsedName = plugin.parseName(name),
-            options = f.extend({}, config.i18next),
-            namespaces = parsedName.namespaces,
-            dir = parsedName.module,
-            url, supportedLngs;
+            var parsedName = plugin.parseName(name), 
+                options = config.i18next, 
+                namespaces = parsedName.namespaces, 
+                module = parsedName.module, 
+                supportedLngs = options.supportedLngs;
 
-            // Store original resource path when running for the first time
-            if (!origResGetPath) {
-                origResGetPath = options.resGetPath || o.resGetPath;
+            // Store default namespace if not done yet
+            if (!defaultNamespace) {
+                defaultNamespace = options.ns || o.ns;
             }
 
-            // Define locale URL
-            url = req.toUrl(dir + (dir.substr(dir.length-1) !== "/" ? "/" : "") + origResGetPath);
-
-            // Define supported languages
-            supportedLngs = options.supportedLngs;
-            if (supportedLngs && supportedLngs[dir]) {
-                supportedLngs = supportedLngs[dir];
+            // Check for scoped supported languages value
+            if (supportedLngs && supportedLngs[module]) {
+                supportedLngs = supportedLngs[module];
             }
 
-            // Set resource path
-            options.resGetPath = url;
+            // Fix module ending slash
+            module += module.substr(module.length - 1) !== "/" ? "/" : "";
 
-            // Set custom load function if supported languages is defined
-            if (supportedLngs) {
-                options.customLoad = function(lng, ns, opts, done) {
-                    var supported;
+            // Backup existing namespaces, so we can restore them when new ones are loaded
+            namespaces.push(defaultNamespace);
+            f.each(resStore, function(lng, nss) {
+                f.each(nss, function(ns) {
+                    if (resStore[lng][ns]) {
+                        resStore[lng]['_' + ns] = resStore[lng][ns];
+                        delete resStore[lng][ns];
+                    }
+                });
+            });
 
-                    // Check for language and namespace support
+            // Set the existing resources
+            options.resStore = resStore;
+
+            // Set a custom load function
+            options.customLoad = function(lng, ns, opts, done) {
+                var supported, url;
+
+                if (supportedLngs) {
                     f.each(supportedLngs, function(language, namespaces) {
                         if (language === lng) {
-                            f.each(namespaces, function(idx, namespace) {
+                            f.each(namespaces,function(idx, namespace) {
                                 return !(supported = namespace === ns);
                             });
                         }
                         return !supported;
                     });
-
-                    // Return if language and/or namespace are not supported
                     if (!supported) {
-                        f.log("no support found for: " + ns + ":" + lng);
+                        f.log("no support found for: " + ns);
                         done(null, {});
                         return;
                     }
+                }
 
-                    // Make the request
-                    f.ajax({
-                        url: f.applyReplacement(url, {lng: lng, ns: ns}),
-                        dataType: "json",
-                        async: opts.async,
-                        success: function(data, status, xhr) {
-                            f.log("loaded: " + url);
-                            done(null, data);
-                        },
-                        error: function(xhr, status, error) {
-                            f.log("failed loading: " + url);
-                            done(error, {});
+                // Define resource url
+                url = req.toUrl(module + f.applyReplacement(opts.resGetPath, {lng : lng, ns : ns}));
+
+                f.ajax({
+                    url : url,
+                    dataType : "json",
+                    async : opts.async,
+                    success : function(data, status, xhr) {
+                        f.log("loaded: " + url);
+                        resStore[lng] = resStore[lng] || {};
+                        resStore[lng][ns] = data;
+                        if (resStore[lng]['_' + ns]) {
+                            f.extend(resStore[lng][ns], resStore[lng]['_' + ns]);
+                            delete resStore[lng]['_' + ns];
                         }
-                    });
-                };
-            }
+                        done(null, resStore[lng][ns]);
+                    },
+                    error : function(xhr, status, error) {
+                        f.log("failed loading: " + url);
+                        done(error, {});
+                    }
+                });
+            };
 
-            // Unset supported languages
+            // Delete supported languages, they are only needed by this plugin
             delete options.supportedLngs;
 
-            // Initialize i18next and return the i18next object
+            // Initialize i18next and load all needed resources
             i18next.init(options, function() {
                 i18next.loadNamespaces(namespaces, function() {
                     onload(i18next);
