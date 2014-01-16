87ec1ce349818dbbc439990b68baa083a8194c54
jcbvm <jacob.v.m@gmail.com>
2014-01-16 20:15:33 +0100

    Fixed #7. Added resources functions to public API

--- i18next.js
+++ i18next.js
@@ -9,59 +9,58 @@ define(["i18next"], function(i18next) {
     "use strict";
 
     var plugin,
-        defaultNamespace,
         supportedLngs,
         resStore = {},
         f = i18next.functions,
         o = i18next.options;
 
-    /**
-     * Checks to see if there exists a resource with the given 
-     * module, language and namespace in the store. 
-     * 
-     * @param {String} module The module name
-     * @param {String} lng The language
-     * @param {String} ns The namespace
-     * @returns {Boolean} If the resource exists in the store
-     */
-    function resourceExists(module, lng, ns) {
-        return resStore[module] && resStore[module][lng] && resStore[module][lng][ns];
-    }
-
-    /**
-     * Adds a resource to the store (overrides existing one).
-     * 
-     * @param {String} module The module name
-     * @param {String} lng The language
-     * @param {String} ns The namespace
-     * @param {Object} data The resource data
-     */
-    function addResource(module, lng, ns, data) {
-        resStore[module] = resStore[module] || {};
-        resStore[module][lng] = resStore[module][lng] || {};
-        resStore[module][lng][ns] = data;
-    }
-
-    /**
-     * Gets all resources by the given language and namespace.
-     * 
-     * @param {String} lng The language
-     * @param {String} ns The namespace
-     * @returns {Object} The resource data
-     */
-    function getResources(lng, ns) {
-        var data = {};
-        f.each(resStore, function(module) {
-            if (resStore[module][lng] && resStore[module][lng][ns]) {
-                f.extend(data, resStore[module][lng][ns]);
-            }
-        });
-        return data;
-    }
-
     plugin = {
         version: "0.3.0",
         pluginBuilder: "./i18next-builder",
+
+        /**
+         * Checks to see if there exists a resource with the given 
+         * module, language and namespace in the store. 
+         * 
+         * @param {String} module The module name
+         * @param {String} lng The language
+         * @param {String} ns The namespace
+         * @returns {Boolean} If the resource exists in the store
+         */
+        resourceExists: function(module, lng, ns) {
+            return resStore[module] && resStore[module][lng] && resStore[module][lng][ns];
+        },
+
+        /**
+         * Adds a resource to the store (overrides existing one).
+         * 
+         * @param {String} module The module name
+         * @param {String} lng The language
+         * @param {String} ns The namespace
+         * @param {Object} data The resource data
+         */
+        addResource: function(module, lng, ns, data) {
+            resStore[module] = resStore[module] || {};
+            resStore[module][lng] = resStore[module][lng] || {};
+            resStore[module][lng][ns] = data;
+        },
+
+        /**
+         * Gets all resources by the given language and namespace.
+         * 
+         * @param {String} lng The language
+         * @param {String} ns The namespace
+         * @returns {Object} The resource data
+         */
+        getResources: function(lng, ns) {
+            var data = {};
+            f.each(resStore, function(module) {
+                if (resStore[module][lng] && resStore[module][lng][ns]) {
+                    f.extend(data, resStore[module][lng][ns]);
+                }
+            });
+            return data;
+        },
 
         /**
          * Parses a resource name into its component parts. 
@@ -86,52 +85,45 @@ define(["i18next"], function(i18next) {
                 module = parsedName.module, 
                 supportedLngs = options.supportedLngs;
 
-            // Store default namespace if not done yet
-            if (!defaultNamespace) {
-                defaultNamespace = options.ns || o.ns;
-            }
-
             // Check for scoped supported languages value
             if (supportedLngs && supportedLngs[module]) {
                 supportedLngs = supportedLngs[module];
             }
 
-            // Set the namespaces
-            namespaces.push(defaultNamespace);
-            options.ns = { 
-                namespaces: namespaces, 
-                defaultNs: defaultNamespace
-            };
+            // Set namespaces
+            if (typeof o.ns == "string") {
+                namespaces.unshift(options.ns || o.ns);
+                options.ns = {
+                    defaultNs: namespaces[0],
+                    namespaces: namespaces
+                };
+            } else {
+                options.ns = o.ns;
+                f.each(namespaces, function(idx, namespace) {
+                    if (options.ns.namespaces.indexOf(namespace) == -1) {
+                        options.ns.namespaces.push(namespace);
+                    }
+                });
+            }
 
             // Set a custom load function
             options.customLoad = function(lng, ns, opts, done) {
-                var supported, url;
 
                 // Check for already loaded resource
-                if (resourceExists(module, lng, ns)) {
-                    done(null, getResources(lng, ns));
+                if (plugin.resourceExists(module, lng, ns)) {
+                    done(null, plugin.getResources(lng, ns));
                     return;
                 }
 
                 // Check for language/namespace support
-                if (supportedLngs) {
-                    f.each(supportedLngs, function(language, namespaces) {
-                        if (language === lng) {
-                            f.each(namespaces, function(idx, namespace) {
-                                return !(supported = namespace === ns);
-                            });
-                        }
-                        return !supported;
-                    });
-                    if (!supported) {
-                        f.log("no support found for: " + ns);
-                        done(null, {});
-                        return;
-                    } 
+                if (supportedLngs && (!supportedLngs[lng] || supportedLngs[lng].indexOf(ns) == -1)) {
+                    f.log("no locale support found for " + lng + " with namespace " + ns);
+                    done(null, {});
+                    return;
                 }
 
                 // Define resource url
-                url = req.toUrl(module + (module.substr(module.length - 1) !== "/" ? "/" : "") + 
+                var url = req.toUrl(module + (module.substr(module.length - 1) !== "/" ? "/" : "") + 
                         f.applyReplacement(opts.resGetPath, {lng : lng, ns : ns}));
 
                 // Make the request
@@ -140,9 +132,9 @@ define(["i18next"], function(i18next) {
                     dataType: "json",
                     async: opts.async,
                     success: function(data, status, xhr) {
-                        addResource(module, lng, ns, data);
+                        plugin.addResource(module, lng, ns, data);
                         f.log("loaded: " + url);
-                        done(null, getResources(lng, ns));
+                        done(null, plugin.getResources(lng, ns));
                     },
                     error: function(xhr, status, error) {
                         f.log("failed loading: " + url);
